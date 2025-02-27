ea6f7ff1fce034ff6c9cba7ac72ce546a6f83bb3
tinchx1 <tinchomarchi34@gmail.com>
2025-02-27 14:23:42 -0300

    refactor: simplify navigation logic by using lodash for debouncing

--- composables/useNavigation.js
+++ composables/useNavigation.js
@@ -1,22 +1,12 @@
-import { debounce } from 'lodash';
+import _ from 'lodash';
 
-export function useNavigation(currentIndex, slidesCopy, updateSlide, Direction) {
-  const nextSlide = debounce(() => {
+export function useNavigation(updateSlide, Direction) {
+  const nextSlide = _.debounce(() => {
     updateSlide(Direction.UP);
-    // if (currentIndex.value < slidesCopy.value.length - 1) {
-    //   currentIndex.value += 1;
-    // } else {
-    //   currentIndex.value = 0;
-    // }
   }, 300);
 
-  const prevSlide = debounce(() => {
+  const prevSlide = _.debounce(() => {
     updateSlide(Direction.DOWN);
-    // if (currentIndex.value > 0) {
-    //   currentIndex.value -= 1;
-    // } else {
-    //   currentIndex.value = slidesCopy.value.length - 1;
-    // }
   }, 300);
 
   return { nextSlide, prevSlide };
